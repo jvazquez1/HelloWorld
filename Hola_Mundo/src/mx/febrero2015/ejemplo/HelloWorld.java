21d3cc492f527c857a904a3085213cc78866658d
Jose Vazquez <vazquez.joseantonio01@gmail.com>
2015-02-14 23:37:06 -0600

    Cambio 14 de febrero 2015

---
+++ Hola_Mundo/src/mx/febrero2015/ejemplo/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package mx.febrero2015.ejemplo;
+
+/**
+ * @author José Antonio P. Vázquez
+ * 14/02/2015 23:34:14
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Github!!");
+	}
+}
